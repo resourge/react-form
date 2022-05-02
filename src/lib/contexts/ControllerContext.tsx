f05c9963d9d239f2dd45a721a519c2150af8a0d3
resourge <jdaoliveiraprograming@hotmail.com>
2022-05-02 16:58:08 +0100

    docs(controller): improve types for useController and useFormField

--- src/lib/contexts/ControllerContext.tsx
+++ src/lib/contexts/ControllerContext.tsx
@@ -5,19 +5,18 @@ import invariant from 'tiny-invariant'
 
 import { FormState, FieldForm } from '../types/types'
 
-export const ControllerContext = createContext<{
-	field: FieldForm
-	formState: FormState<any>
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-}>(null!);
-
-export const useController = <T extends object>(): {
-	field: FieldForm
+export type ControllerField<T extends Record<string, any>, Value = any> = {
+	field: FieldForm<Value>
 	formState: FormState<T>
-} => {
+}
+
+// @ts-expect-error I want the validation on the useController
+export const ControllerContext = createContext<ControllerField<any>>(null);
+
+export const useController = <T extends Record<string, any>, Value = any>(): ControllerField<T, Value> => {
 	const context = useContext(ControllerContext)
 
 	invariant(context, 'useControllerContext can only be used in the context of a <Controller> component.')
 
-	return context
+	return context as ControllerField<T, Value>
 }
--- src/lib/contexts/index.ts
+++ src/lib/contexts/index.ts
@@ -1,9 +1,11 @@
 import { ControllerContext, useController } from './ControllerContext';
+import type { ControllerField } from './ControllerContext';
 import { FormContext, useFormContext } from './FormContext';
 
 export {
 	ControllerContext, 
 	useController,
+	ControllerField,
 
 	FormContext,
 	useFormContext
