1133f44f23c6e20a33dd91bbb0f2762401a17837
resourge <jdaoliveiraprograming@hotmail.com>
2023-02-02 10:32:38 +0000

    fix(classdecorators): fix type not working in every class

--- src/lib/utils/classDecorators.ts
+++ src/lib/utils/classDecorators.ts
@@ -16,7 +16,8 @@ const addClassDecorator = (target: any, propertyKey: string, value: any) => {
 /**
  * Serves to preserve the class inside an array when converting from JSON to Data
  */
-export function PreserveArrayClass(value: new() => any ) {
+// eslint-disable-next-line @typescript-eslint/prefer-function-type
+export function PreserveArrayClass(value: { new(...args: any[]): any }) {
 	return (target: any, propertyKey: string) => {
 		addClassDecorator(target, `${propertyKey}[*]`, value)
 	};
@@ -25,7 +26,8 @@ export function PreserveArrayClass(value: new() => any ) {
 /**
  * Serves to preserve the class when converting from JSON to Data
  */
-export function PreserveClass(value: new() => any ) {
+// eslint-disable-next-line @typescript-eslint/prefer-function-type
+export function PreserveClass(value: { new(...args: any[]): any }) {
 	return (target: any, propertyKey: string) => {
 		addClassDecorator(target, propertyKey, value)
 	};
