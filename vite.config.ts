9439a865b8a52c318d07ebd9aa42d19ecaaa3bbd
resourge <jdaoliveiraprograming@hotmail.com>
2024-07-08 12:40:32 +0100

    chore(package.json): update libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
 /// <reference types="vitest" />
 
 import react from '@vitejs/plugin-react';
-import checker from 'vite-plugin-checker';
+// import checker from 'vite-plugin-checker';
 
 import { defineLibConfig } from './config/defineLibConfig';
 
 // https://vitejs.dev/config/
 export default defineLibConfig(() => ({
 	plugins: [
-		react(),
-		checker({ 
+		react()
+		/* checker({ 
 			typescript: true,
 			enableBuild: true,
 			overlay: {
 				initialIsOpen: false
 			},
-			eslint: {
-				lintCommand: 'eslint "./src/**/*.{ts,tsx}"'
-			}
-		})
+			eslint: { */
+		// lintCommand: 'eslint "./src/**/*.{ts,tsx}"'
+		/* }
+		}) */
 	]
 }));
