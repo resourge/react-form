f5ce4cb16ea8abb2628e5a94eecf445f465fce1c
resourge <jdaoliveiraprograming@hotmail.com>
2025-02-28 11:40:12 +0000

    refactor(app.tsx): remove test

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,8 @@
-import { type FC } from 'react';
-
-import { FormProvider, useForm, useFormSplitter } from './lib';
-
-type Props = {
-};
-
-const Asd = () => {
-	const { form, reset } = useFormSplitter('test.test');
-	
-	return (
-		<button
-			onClick={() => {
-				const v = form.at(0);
-
-				reset([...form, {
-					...v,
-					id: 10 
-				}]);
-			}}
-		>
-			Reset
-		</button>
-	);
-};
-
 function App() {
-	const { context } = useForm(
-		{
-			test: {
-				test: []
-			}
-		}, 
-		{
-			validate: (form) => {
-				const val = form.test.test[0];
-				console.log('val', val);
-				const val1 = form.test.test[1];
-				console.log('val1', val1);
-	
-				return [];
-			},
-			validationType: 'onTouch'
-		}
-	);
-
 	return (
-		<FormProvider context={context}>
-			<Asd />
-		</FormProvider>
+		<div>
+			App
+		</div>
 	);
 }
 
